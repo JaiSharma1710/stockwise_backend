88bdcc9d5ed0ebfaa780e7d3b5c9cba412a22860
JaiSharma <sharmajai901@gmail.com>
2024-05-15 23:59:40 +0530

    changes

--- controller/ratio.controller.js
+++ controller/ratio.controller.js
@@ -1,19 +1,34 @@
-const { getCompanyRatios } = require("../helper/ratio.helper");
+const {
+  getCompanyRatios,
+  getSectorRatios,
+  addGrowth,
+} = require('../helper/ratio.helper');
 
 async function calculateRatios(req, res) {
   try {
     const { symbol } = req.query;
 
     if (!symbol) {
-      throw new Error("no company symbol found");
+      throw new Error('no company symbol found');
     }
 
-    const data = await getCompanyRatios(symbol);
-    res.status(200).json({ status: "success", data: data });
+    const companyData = await getCompanyRatios(symbol);
+
+    // const sectorData = await getSectorRatios(symbol);
+
+    const Growth = addGrowth(companyData);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        companyData,
+        Growth,
+      },
+    });
   } catch (error) {
     res.status(500).json({
-      status: "failed",
-      message: error.message || "something went wrong",
+      status: 'failed',
+      message: error.message || 'something went wrong',
     });
   }
 }
--- helper/ratio.helper.js
+++ helper/ratio.helper.js
@@ -1,15 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const schemaCommon = new mongoose.Schema({}, { strict: false });
+
 const incomStatement = mongoose.model(
-  "incomeStatement_data",
+  'incomeStatement_data',
   schemaCommon,
-  "incomeStatement_data"
+  'incomeStatement_data'
 );
 const balanceSheet = mongoose.model(
-  "balanceSheet_data",
+  'balanceSheet_data',
   schemaCommon,
-  "balanceSheet_data"
+  'balanceSheet_data'
 );
+
+const basicInfo = mongoose.model('basic_information');
 
 async function getCompanyRatios(symbol) {
   const [incomeStatementData, balanceSheetDate] = await Promise.all([
@@ -18,13 +21,13 @@ async function getCompanyRatios(symbol) {
   ]);
 
   const productEffeciency = calculateProductEffeciency(
-    incomeStatementData["Operating Income"],
-    incomeStatementData["Total Revenue"]
+    incomeStatementData['Operating Income'],
+    incomeStatementData['Total Revenue']
   );
 
   const oprationEffeciency = calculateOprationEffeciency(
-    incomeStatementData["Pretax Income"],
-    incomeStatementData["Operating Income"]
+    incomeStatementData['Pretax Income'],
+    incomeStatementData['Operating Income']
   );
 
   const totalEffeciency = calculateTotalEffeciency(
@@ -33,43 +36,128 @@ async function getCompanyRatios(symbol) {
   );
 
   const profitabilityRatio = calculateProfitabilityRatio(
-    incomeStatementData["Net Income"],
-    incomeStatementData["Total Revenue"]
+    incomeStatementData['Net Income'],
+    incomeStatementData['Total Revenue']
   );
 
   const fixedAssetTurnoverRatio = calculateFixedAssetTurnoverRatio(
-    incomeStatementData["Total Revenue"],
-    balanceSheetDate["Total Non Current Assets"]
+    incomeStatementData['Total Revenue'],
+    balanceSheetDate['Net PPE']
   );
 
   const inventoryTurnOverRatio = calculateInventoryTurnOverRatio(
-    incomeStatementData["Total Revenue"],
-    balanceSheetDate["Inventory"]
+    incomeStatementData['Total Revenue'],
+    balanceSheetDate['Inventory']
   );
 
   const totalAssetTurnoverRatio = calculateTotalAssetTurnoverRatio(
-    incomeStatementData["Total Revenue"],
-    balanceSheetDate["Total Assets"]
+    incomeStatementData['Total Revenue'],
+    balanceSheetDate['Total Assets']
   );
 
   const returnOnAsset = calculateReturnOnAsset(
-    incomeStatementData["Net Income"],
-    balanceSheetDate["Total Assets"]
+    incomeStatementData['Net Income'],
+    balanceSheetDate['Total Assets']
   );
 
-  const eps = incomeStatementData["Basic EPS"];
+  const eps = incomeStatementData['Basic EPS'];
+  addCagr(eps);
+  eps.Weightage = '10%';
+
+  const leverage = calculateLeverage(
+    balanceSheetDate['Total Assets'],
+    balanceSheetDate['Stockholders Equity']
+  );
+
+  const returnOnEquity = calculateReturnOnEquity(
+    incomeStatementData['Net Income Common Stockholders'],
+    balanceSheetDate['Stockholders Equity']
+  );
 
   return {
-    "Production Efficiency": productEffeciency,
-    "Operation Efficiency": oprationEffeciency,
-    "Total Efficiency": totalEffeciency,
-    "Profitability Ratio": profitabilityRatio,
-    "Fixed Asset Turnover Ratio": fixedAssetTurnoverRatio,
-    "Inventory Turnover Ratio": inventoryTurnOverRatio,
-    "Total Asset Turnover Ratio": totalAssetTurnoverRatio,
-    "Return on Asset": returnOnAsset,
+    'Production Efficiency': productEffeciency,
+    'Operation Efficiency': oprationEffeciency,
+    'Total Efficiency': totalEffeciency,
+    'Profitability Ratio': profitabilityRatio,
+    'Fixed Asset Turnover Ratio': fixedAssetTurnoverRatio,
+    'Inventory Turnover Ratio': inventoryTurnOverRatio,
+    'Total Asset Turnover Ratio': totalAssetTurnoverRatio,
+    'Return on Asset': returnOnAsset,
     EPS: eps,
+    Leverage: leverage,
+    'Return On Equity (ROE)': returnOnEquity,
   };
+}
+
+async function getSectorRatios(symbol) {
+  const companiesSector = await basicInfo.findOne(
+    { symbol },
+    { sector: 1, _id: 0 }
+  );
+
+  if (!companiesSector.sector) {
+    throw new Error('no sector for selected company');
+  }
+
+  const companiesInSector = await basicInfo.find(companiesSector, {
+    symbol: 1,
+    _id: 0,
+  });
+
+  if (!companiesInSector.length) {
+    throw new Error('no companies in selected sector');
+  }
+
+  const allDataPromise = companiesInSector.map((company) =>
+    getCompanyRatios(company.symbol)
+  );
+
+  const allData = await Promise.all(allDataPromise);
+
+  const years = await getCompanyYears(symbol);
+
+  if (!years.length) {
+    throw new Error('no data for selected company');
+  }
+
+  const ratioData = {
+    'Production Efficiency': {},
+    'Operation Efficiency': {},
+    'Total Efficiency': {},
+    'Profitability Ratio': {},
+    'Fixed Asset Turnover Ratio': {},
+    'Inventory Turnover Ratio': {},
+    'Total Asset Turnover Ratio': {},
+    'Return on Asset': {},
+    EPS: {},
+  };
+
+  allData.forEach((company) => {
+    Object.keys(ratioData).forEach((key) => {
+      Object.keys(company[key]).forEach((year) => {
+        const currentYear = new Date().getFullYear();
+        const dataYear = new Date(year).getFullYear();
+        if (currentYear - dataYear < 4) {
+          ratioData[key][year] = ratioData[key][year]
+            ? +((ratioData[key][year] + company[key][year]) / 2).toFixed(2)
+            : +company[key][year].toFixed(2);
+        }
+      });
+    });
+  });
+
+  return ratioData;
+}
+
+async function getCompanyYears(symbol) {
+  const data = await balanceSheet.findOne(
+    { symbol },
+    { 'Ordinary Shares Number': 1, _id: 0 }
+  );
+  if (data && data?.['Ordinary Shares Number']) {
+    return Object.keys(data['Ordinary Shares Number']);
+  }
+  return [];
 }
 
 function calculateProductEffeciency(operatingIncome, totalRevenue) {
@@ -86,6 +174,9 @@ function calculateProductEffeciency(operatingIncome, totalRevenue) {
       );
     }
   });
+
+  addCagr(result);
+  result.Weightage = '5%';
 
   return result;
 }
@@ -105,6 +196,9 @@ function calculateOprationEffeciency(pretaxIncome, operatingIncome) {
     }
   });
 
+  addCagr(result);
+  result.Weightage = '5%';
+
   return result;
 }
 
@@ -123,6 +217,9 @@ function calculateTotalEffeciency(productEffeciency, oprationEffeciency) {
     }
   });
 
+  addCagr(result);
+  result.Weightage = '5%';
+
   return result;
 }
 
@@ -138,6 +235,9 @@ function calculateProfitabilityRatio(netIncome, totalRevenue) {
       result[year] = Number((netIncome[year] / totalRevenue[year]).toFixed(3));
     }
   });
+
+  addCagr(result);
+  result.Weightage = '10%';
 
   return result;
 }
@@ -157,6 +257,9 @@ function calculateFixedAssetTurnoverRatio(totalRevenue, fixedAssets) {
     }
   });
 
+  addCagr(result);
+  result.Weightage = '5%';
+
   return result;
 }
 
@@ -172,6 +275,9 @@ function calculateInventoryTurnOverRatio(totalRevenue, inventory) {
       result[year] = Number((totalRevenue[year] / inventory[year]).toFixed(3));
     }
   });
+
+  addCagr(result);
+  result.Weightage = '5%';
 
   return result;
 }
@@ -191,6 +297,9 @@ function calculateTotalAssetTurnoverRatio(totalRevenue, totalAssets) {
     }
   });
 
+  addCagr(result);
+  result.Weightage = '10%';
+
   return result;
 }
 
@@ -207,9 +316,87 @@ function calculateReturnOnAsset(netIncome, totalAssets) {
     }
   });
 
+  addCagr(result);
+  result.Weightage = '10%';
+
   return result;
+}
+
+function calculateLeverage(totalAssets, shareHolderEquity) {
+  const keys = Object.keys(totalAssets);
+
+  const result = {};
+
+  keys.forEach((year) => {
+    if (!totalAssets?.[year] || !shareHolderEquity?.[year]) {
+      result[year] = 0;
+    } else {
+      result[year] = Number(
+        (totalAssets[year] / shareHolderEquity[year]).toFixed(3)
+      );
+    }
+  });
+
+  addCagr(result);
+  result.Weightage = '10%';
+
+  return result;
+}
+
+function calculateReturnOnEquity(
+  NetIncomeCommonStockholders,
+  shareHolderEquity
+) {
+  const keys = Object.keys(NetIncomeCommonStockholders);
+
+  const result = {};
+
+  keys.forEach((year) => {
+    if (!NetIncomeCommonStockholders?.[year] || !shareHolderEquity?.[year]) {
+      result[year] = 0;
+    } else {
+      result[year] = Number(
+        (NetIncomeCommonStockholders[year] / shareHolderEquity[year]).toFixed(3)
+      );
+    }
+  });
+
+  addCagr(result);
+  result.Weightage = '5%';
+
+  return result;
+}
+
+function addCagr(ratio) {
+  const keys = Object.keys(ratio);
+  const years = keys.sort((a, b) => a - b);
+
+  const latestYear = years[0];
+  const firstYear = years.at(-1);
+  const numberOfYears = years.length;
+
+  ratio.CAGR = `${+(
+    ((ratio[latestYear] / ratio[firstYear]) ** (1 / (numberOfYears - 1)) - 1) *
+    100
+  ).toFixed(3)}%`;
+}
+
+function addGrowth(result) {
+  const ratios = Object.keys(result);
+  let Growth = 0;
+  ratios.forEach((ratio) => {
+    const { CAGR, Weightage } = result[ratio];
+    const CARG_Value = +CAGR.split('%')[0];
+    const Weightage_Value = +Weightage.split('%')[0];
+    if (!!CARG_Value && !!Weightage) {
+      Growth += (CARG_Value / 100) * (Weightage_Value / 100);
+    }
+  });
+  return Growth * 100;
 }
 
 module.exports = {
   getCompanyRatios,
+  addGrowth,
+  getSectorRatios,
 };
